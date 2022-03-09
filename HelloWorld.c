bf390bf5bc261165c978b5f60eac583341389928
ElladanH <ericryley93@yahoo.com>
2022-03-09 22:13:05 +0000

    Hello World exercise practice with first commit.

---
+++ HelloWorld.c
@@ -0,0 +1,11 @@
+#include <stdio.h> //including the standard input/output library to get printf
+
+// this is a comment
+
+// the main function where the execution of the program begins
+int main()
+{
+    printf("hello world!");
+    // 0 default value means that program executed successfully
+    return 0;
+}
